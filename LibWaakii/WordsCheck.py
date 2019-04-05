ceab06a74fdf85bb287822cf8cbf93e4d35e1109
jeffreyshou7 <jeffreyshou@163.com>
2019-04-05 17:25:47 +0800

    完成度90%

--- LibWaakii/WordsCheck.py
+++ LibWaakii/WordsCheck.py
@@ -10,8 +10,10 @@ class RegexChecker(object):
         判断一个字符串是否是合法IP地址
         '''
         oRe = re.compile('^((25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(25[0-5]|2[0-4]\d|[01]?\d\d?)$')
-
-        if oRe.match(ip_str):  
-            return True  
-        else:  
+        try:
+            if oRe.match(ip_str):  
+                return True  
+            else:  
+                return False
+        except:
             return False
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: UTF-8 -*-
  
 import json
@@ -20,25 +19,131 @@ import LibWaakii.AliYunDns as DDNS
 class Worker(object):
 
     _cfg = {}
-    _DDNS = object
+    _region_id = 'cn-hangzhou'
+    _last_ip = None
+    #_DDNS = object
+    #_TimerWorker = object 
+
+    @classmethod
+    def resetDDNS(cls):
+        cls._DDNS = DDNS.DNSWorker(cls._cfg['domain'],cls._cfg['access_key_id'],cls._cfg['access_Key_secret'],cls._region_id)
+        
+        if cls._DDNS.get_record_all() != None:
+            AppLogger.StandLogger.infoLog('与阿里云API服务通信成功！')
+            return True
+        else:
+            AppLogger.StandLogger.infoLog('与阿里云API服务通信失败，请检查AppID/Key后重新启动服务,系统将自动退出！')
+            return False
+
+    @classmethod
+    def getRRValue(cls):
+        sIp = None
+
+        if cls._DDNS.get_record_all() != None:
+            try:
+                sIp = cls._DDNS.get_record_value(cls._cfg['rr'])['Value']
+            except:
+                sIp = None
+        return sIp
+
+    @classmethod
+    def getGatewayIP(cls):
+        oIpAddress = IpInfo.IpAddress()
+        sGatewayIp = oIpAddress.getGatewayIp()
+
+        if sGatewayIp != None:
+            AppLogger.StandLogger.infoLog('取得外网IP成功,ip地址为(' + sGatewayIp + ')')
+            return sGatewayIp
+        else:
+            AppLogger.StandLogger.warningLog('取得外网IP失败，可能原因（网络未连接）')
+            return None
+
+    @classmethod
+    def getCfgLastIp(cls):
+        try:
+            return cls._cfg['last_ip']
+        except:
+            return None
+
+    @classmethod
+    def WorkerInit(cls):
+        cls._cfg = AppConfig.JsonConf().load()
+        # 服务启动后进行一次域名解析的校验动作
+        bRc = cls.resetDDNS()
+
+        if True == bRc:
+            sRRIp = cls.getRRValue()
+            sGatewayIp = cls.getGatewayIP()
+            sCfgIp = cls.getCfgLastIp()
+
+            if None == sGatewayIp:
+                AppLogger.StandLogger.warningLog('获取外网IP失败，网络连接有故障，请检查')
+                return False
+
+            cls._last_ip = sGatewayIp
+
+            if sGatewayIp != sCfgIp:
+                AppConfig.JsonConf().set({'last_ip':sGatewayIp})
+                AppConfig.JsonConf().set({'last_update':datetime.now().strftime("%Y-%m-%d %H:%M:%S")})
+
+            if sRRIp == sGatewayIp:       
+                return True
+            else:
+                bUpdated = cls._DDNS.update_record(cls._cfg['rr'],sGatewayIp)
+
+                if True == bUpdated:
+                    AppLogger.StandLogger.infoLog('网关IP已变化，在系统初始化中已通过阿里云API成功更新')
+                else:
+                    AppLogger.StandLogger.warningLog('网关IP已变化，但通过阿里云API失败')
+
+                return bUpdated
+        else:
+            return False
 
     @classmethod
     def start(cls):
-        cls._cfg = AppConfig.JsonConf().load()
-        cls._DDNS = DDNS.DNSWorker(cls._cfg['domain'],cls._cfg['access_key_id'],cls._cfg['access_Key_secret'],'cn-hangzhou')
-
-        if cls._DDNS.get_record_all() != None:
-            AppLogger.StandLogger.infoLog('与阿里云API服务通信成功！')
+        cls._is_inited = cls.WorkerInit()
+        if True == cls._is_inited:
+            AppLogger.StandLogger.infoLog('系统初始化工作成功')
         else:
-            AppLogger.StandLogger.infoLog('与阿里云API服务通信失败，请检查AppID/Key后重新启动服务,系统将自动退出！')
-            AppBase.appExit()
-        
-        oIpAddress = IpInfo.IpAddress()
-        sGatewayIp = oIpAddress.getGatewayIp()
-        AppLogger.StandLogger.infoLog('取得外网IP成功,ip地址为(' + sGatewayIp + ')')
+            AppLogger.StandLogger.warningLog('系统初始化工作失败，将会在下一工作周期重试')
 
-        if WordsCheck.RegexChecker.judgeLegalIpv4(cls._cfg['last_ip']) and cls._cfg['last_ip'] != sGatewayIp:
-            AppConfig.JsonConf().set({'last_ip':sGatewayIp})
+        cls._TimerWorker = TimerWorker.ScheduleTimer(datetime.now().replace( minute=3, second=0, microsecond=0),int(cls._cfg['interval']),cls.ScheduleWork)
+        cls._TimerWorker.start()
+
+    @classmethod
+    def ScheduleWork(cls):
+        print(datetime.now())
+        if False == cls._is_inited:
+            AppLogger.StandLogger.infoLog('系统再次尝试初始化工作')
+            cls._is_inited = cls.WorkerInit()
+        pass
+
+        if True == cls._is_inited:
+            sGatewayIp = cls.getGatewayIP()
+            if sGatewayIp != cls._last_ip:
+                cls._last_ip = sGatewayIp
+                AppConfig.JsonConf().set({'last_ip':sGatewayIp})
+                AppConfig.JsonConf().set({'last_update':datetime.now().strftime("%Y-%m-%d %H:%M:%S")})
+                
+                bUpdated = cls._DDNS.update_record(cls._cfg['rr'],sGatewayIp)
+
+                if True == bUpdated:
+                    AppLogger.StandLogger.infoLog('网关IP已变化，已通过阿里云API成功更新')
+                else:
+                    AppLogger.StandLogger.warningLog('网关IP已变化，但通过阿里云API成功失败')
+            else:
+                AppLogger.StandLogger.infoLog('网关IP无变化！')
+        '''     
+        dicAliDnsRecord = cls._DDNS.get_record_value(cls._cfg['rr'])
+        if dicAliDnsRecord != None:
+            if dicAliDnsRecord['Value'] != cls._cfg['last_ip']:
+                print(dicAliDnsRecord)
+        else
+        '''
+        #if WordsCheck.RegexChecker.judgeLegalIpv4(cls._cfg['last_ip']) and cls._cfg['last_ip'] != sGatewayIp:
+            #AppConfig.JsonConf().set({'last_ip':sGatewayIp})
+    
     #@classmethod
     #def scheduleWork(cls):
         
@@ -53,7 +158,7 @@ def main():
     #_config = dicAppConfig = oAppConfig.load()
     #print(_config)
     #print('ok')
-    #print(datetime.now().replace( minute=3, second=0, microsecond=0 ))
+    #print(datetime.now().replace( minute=3, second=0, microsecond=0))
     #oTimer = TimerWorker.ScheduleTimer(datetime.now(),5,test,'p')
     #oTimer.start()
 
