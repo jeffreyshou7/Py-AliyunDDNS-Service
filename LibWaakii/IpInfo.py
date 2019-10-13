72d47cdc1b3dd41fe5e1c34d632f265f730bac8f
Jeffrey Shou <jeffreyshou@163.com>
2019-10-13 16:57:12 +0800

    参考网友建议，简化获取IP的方式

--- LibWaakii/IpInfo.py
+++ LibWaakii/IpInfo.py
@@ -1,42 +1,22 @@
 #-*- coding:utf-8 -*-
  
 import requests
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 import re
 import LibWaakii.AppLoggerLite as AppLogger
 import time
 import LibWaakii.AppConfig as AppConfig
 
 class IpAddress(object):
-    # 获取外网IP
-    def parseOutIp(self,content):
-        try:
-            oContent = requests.get(content)
-
-
-            if oContent != None:
-                sHtmlContent = oContent.text
-                ip = sHtmlContent[sHtmlContent.find("[") + 1: sHtmlContent.find("]")]
-                
-                # AppLogger.StandLogger.infoLog('取得外网IP成功,ip地址为(' + ip + ')')
-
-                AppConfig.JsonConf().set({'last_ip':ip})
-                
-                return ip
-            else:
-                # AppLogger.StandLogger.warningLog('取得外网IP失败，可能原因（网络未连接）')
-                return None
-        except:
-            return None
-
-    def getIpServiceContent(self, url = r'http://www.ip138.com/'):
+    
+    #def getIpServiceContent(self, url = r'http://www.ip138.com/'):
+    def getIpServiceContent(self, url = 'https://ifconfig.me/ip'):
         try:
             r = requests.get(url)
-            txt = r.text
-            soup = BeautifulSoup(txt,"html.parser").iframe
-            return soup["src"]
+            return r.text
         except:
             return None
 
+   # 获取外网IP
     def getGatewayIp(self):
-        return self.parseOutIp(self.getIpServiceContent())
+        return self.getIpServiceContent()
